44693002408e4e31a46ece3983ba3817967aeafe
miguel <miguel.plopez1000@gmail.com>
2021-06-02 09:21:26 +0200

    mis camvios

--- controllers/pages.js
+++ controllers/pages.js
@@ -5,7 +5,12 @@ let routes={
 home:(req,res)=>{
     res.status(200).render('home')
 },
-films:(req,res)=>{},
+film:(req,res)=>{
+    let datos= {
+nombre:"klk esta e tu pelikul"
+    }
+    res.status(200).render('film',datos)
+},
 post:(req,res)=>{},
 
 }
