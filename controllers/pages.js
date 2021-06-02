62fe335ed6fb91156158d12a588efb08361818f7
Juanma Barcia <37333385+JuanmaBarcia@users.noreply.github.com>
2021-06-02 11:49:18 +0200

    CSS + apiKey

--- controllers/pages.js
+++ controllers/pages.js
@@ -7,11 +7,12 @@ const routes = {
     },
     film: async(req, res) => {
         let title = req.params.title
-        let data = await films.getPelicula(`http://www.omdbapi.com/?t=${title}${apiKey}`)
+        let data = await films.getPelicula(`http://www.omdbapi.com/?t=${title}&apikey=${apiKey}`)
         res.status(200).render('film', data)
     },
-    post: async(req, res) => {
+    post: (req, res) => {
         let title = req.body.title
+        console.log(req)
         res.status(200).redirect(`film/${title}`);
     },
     page404: (req, res) => {
