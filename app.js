a59ccb84f33bfa675a36d2e6db64374a9cacf927
Juanma Barcia <37333385+JuanmaBarcia@users.noreply.github.com>
2021-06-03 18:26:56 +0200

    modificacion port

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = require("./controllers/index")
 const routerApi = require("./controllers/apis")
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
 
 app.set('view engine', 'pug');
 app.set('views', './views');
