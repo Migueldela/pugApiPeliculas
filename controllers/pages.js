a49abb24fac987cf7f6cf2e24d324e18f8403849
Juanma Barcia <37333385+JuanmaBarcia@users.noreply.github.com>
2021-06-04 13:54:12 +0200

    correccion variable de entorno

--- controllers/pages.js
+++ controllers/pages.js
@@ -8,7 +8,7 @@ const routes = {
     },
     film: async(req, res) => {
         let title = req.params.title
-        let data = await films.getPelicula(`http://www.omdbapi.com/?t=${title}${apiKey}`)
+        let data = await films.getPelicula(`http://www.omdbapi.com/?t=${title}&apikey=${apiKey}`)
         res.status(200).render('film', data)
     },
     post: async(req, res) => {
